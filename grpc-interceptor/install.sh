12ed81878b761f5cd718e38467c372f040c914c5
Dmitry Kondratyev <dmitry.kondratyev@cern.ch>
2024-05-07 16:35:07 -0400

    improved rejection logic

--- grpc-interceptor/install.sh
+++ grpc-interceptor/install.sh
@@ -1,6 +1,7 @@
 k delete configmap envoy-config -n cms
-k delete deployment envoy-proxy -n cms 
-k delete svc envoy-proxy -n cms
+k delete deployment triton-run2 -n cms 
+k delete svc triton-run2 -n cms
+
 
 k create configmap envoy-config --from-file=envoy.yaml=envoy.yaml -n cms
 k apply -f envoy-resources.yaml -n cms
