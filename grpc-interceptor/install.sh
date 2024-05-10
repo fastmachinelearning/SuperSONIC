0b06f6fcdfbbaf902fb723d5be6066ff41796171
Dmitry Kondratyev <dmitry.kondratyev@cern.ch>
2024-05-10 13:26:07 -0400

    move Lua script in its own file

--- grpc-interceptor/install.sh
+++ grpc-interceptor/install.sh
@@ -1,7 +1,9 @@
 k delete configmap envoy-config -n cms
 k delete deployment triton-run2 -n cms 
 k delete svc triton-run2 -n cms
-
+k delete configmap lua-config -n cms
 
 k create configmap envoy-config --from-file=envoy.yaml=envoy.yaml -n cms
 k apply -f envoy-resources.yaml -n cms
+k create configmap lua-config --from-file=envoy-filter.lua -n cms
+
