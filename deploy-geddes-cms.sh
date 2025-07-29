7cc7f109f3cf9dcd06cac86a80f594dd601f5e19
kondratyevd <kondratyev.d.95@gmail.com>
2025-07-29 17:47:51 -0400

    delete obsolete installation scripts

--- deploy-geddes-cms.sh
+++
@@ -1,2 +0,0 @@
-helm plugin install https://github.com/fastmachinelearning/SuperSONIC/
-helm install-supersonic supersonic -n cms -f values/values-geddes-cms.yaml
--- deploy-nautilus-atlas.sh
+++
@@ -1,2 +0,0 @@
-helm plugin install https://github.com/fastmachinelearning/SuperSONIC/
-helm install-supersonic supersonic -n atlas-sonic -f values/values-nautilus-atlas.yaml
